275ec691d01fd25972855bc009e97ba2fb4059f0
AnlorNode <pgs47@yandex.ua>
2018-04-02 17:32:35 +0300

    bec

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# AutoExchanger
+# my_mongo
+git clone https://github.com/slonik6663444/adspoiler.git
+
+cd ./adspoiler
+
+app `npm i`
+
+start app `npm run start`
+
+open linc [http://localhost:3000/](http://localhost:3000/)
---
+++ server/index.js
@@ -0,0 +1,20 @@
+var static = require("node-static");
+var file = new static.Server("./server/public");
+const port = process.env.PORT || 3000;
+var app = require("http").createServer(handler);
+var io = require("socket.io")(app);
+var fs = require("fs");
+app.listen(port);
+function handler(req, res) {
+  file.serve(req, res, function name(err, dt) {
+    if (err) {
+      console.log(req.url, "err")
+      res.end()
+    };
+  });
+}
+// io.on("connection", function (socket) {
+//   socket.on("shipped", function (data) {
+
+//   });
+// });
