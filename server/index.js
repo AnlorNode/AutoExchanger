1779487be5985093e0ac331dbef68b28e57a7567
AnlorNode <pgs47@yandex.ua>
2018-04-18 02:54:14 +0300

    tream

--- server/index.js
+++ server/index.js
@@ -1,27 +1,27 @@
 var static = require("node-static");
-var file = new static.Server("./server/public");
 const port = process.env.PORT || 3000;
 var app = require("http").createServer(handler);
-var io = require("socket.io")(app);
 var fs = require("fs");
 app.listen(port);
-function handler(req, res) {
-  file.serve(req, res, function name(err, dt) {
-    if (err) {
-      console.log(req.url, "url")
-        var S_tream = fs.createReadStream(__dirname+"/public/index.html").pipe(res)
-        S_tream.on('close', function () {
-          res.end();
-          return;
-        })
-    };
-  });
-
+function handler(req, res) { 
+if (req.url==="/") {
+  var S_tream = fs.createReadStream(__dirname+"/public/index.html").pipe(res)
+    S_tream.on('close', function () {
+      res.end();
+      return;
+    })
+} else {
+  if (fs.existsSync(__dirname+req.url)) {
+    var S_tream = fs.createReadStream(__dirname+req.url).pipe(res)
+    S_tream.on('close', function () {
+      res.end();
+      return;
+    })
+  } else {
+    console.log(req.url,"err");
+    res.end();
+  }
+}
 }
 console.log("open port:",port);
 
-// io.on("connection", function (socket) {
-//   socket.on("shipped", function (data) {
-
-//   });
-// });
